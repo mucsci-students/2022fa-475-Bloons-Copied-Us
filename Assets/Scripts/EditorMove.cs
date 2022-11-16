4eb25f3536eda24347fd0be0cca93931243a8dd3
Jmbrenn <89528880+Jmbrenn@users.noreply.github.com>
2022-11-15 20:00:16 -0500

    modified camera movement

--- Assets/Scripts/EditorMove.cs
+++ Assets/Scripts/EditorMove.cs
@@ -6,12 +6,17 @@ public class EditorMove : MonoBehaviour
     [SerializeField] float sensitivity = 1.0f;
     [SerializeField] float scrollSpeed = 1.0f;
  
+    private Vector3 initial_pos;
     Camera cam;
     Vector3 anchorPoint;
     Quaternion anchorRot;
- 
+    Quaternion initial_rot;
+
+
     private void Awake()
     {
+        initial_pos = transform.position;
+        initial_rot = transform.rotation;
         cam = GetComponent<Camera>();
     }
    
@@ -21,14 +26,19 @@ public class EditorMove : MonoBehaviour
             transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
 
         Vector3 move = Vector3.zero;
+        if(Input.GetKey(KeyCode.R))
+        {
+            transform.position = initial_pos;
+            transform.rotation = initial_rot;
+        }
         if(Input.GetKey(KeyCode.W))
             move += Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.A))
+            move -= Vector3.right * speed;
         if (Input.GetKey(KeyCode.S))
             move -= Vector3.forward * speed;
         if (Input.GetKey(KeyCode.D))
             move += Vector3.right * speed;
-        if (Input.GetKey(KeyCode.A))
-            move -= Vector3.right * speed;
         if (Input.GetKey(KeyCode.E))
             move += Vector3.up * speed;
         if (Input.GetKey(KeyCode.Q))
