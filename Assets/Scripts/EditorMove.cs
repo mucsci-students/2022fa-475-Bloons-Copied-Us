dd17dd912f58aa8a8892e8351ae54cb978031082
Jmbrenn <89528880+Jmbrenn@users.noreply.github.com>
2022-11-15 20:14:24 -0500

    modified camera movement

--- Assets/Scripts/EditorMove.cs
+++ Assets/Scripts/EditorMove.cs
@@ -7,17 +7,21 @@ public class EditorMove : MonoBehaviour
     [SerializeField] float scrollSpeed = 1.0f;
  
     private Vector3 initial_pos;
-    Camera cam;
-    Vector3 anchorPoint;
-    Quaternion anchorRot;
     Quaternion initial_rot;
+
+    Camera camera = new Camera();
+    Vector3 curr;
+    Quaternion Rotation;
+    Quaternion Rotation_amount;
+    
+    
 
 
     private void Awake()
     {
         initial_pos = transform.position;
         initial_rot = transform.rotation;
-        cam = GetComponent<Camera>();
+        camera = GetComponent<Camera>();
     }
    
     void FixedUpdate()
@@ -25,37 +29,37 @@ public class EditorMove : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") != 0) 
             transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
 
-        Vector3 move = Vector3.zero;
+        Vector3 direction = Vector3.zero;
         if(Input.GetKey(KeyCode.R))
         {
             transform.position = initial_pos;
             transform.rotation = initial_rot;
         }
         if(Input.GetKey(KeyCode.W))
-            move += Vector3.forward * speed;
+            direction += Vector3.forward * speed;
         if (Input.GetKey(KeyCode.A))
-            move -= Vector3.right * speed;
+            direction -= Vector3.right * speed;
         if (Input.GetKey(KeyCode.S))
-            move -= Vector3.forward * speed;
+            direction -= Vector3.forward * speed;
         if (Input.GetKey(KeyCode.D))
-            move += Vector3.right * speed;
+            direction += Vector3.right * speed;
         if (Input.GetKey(KeyCode.E))
-            move += Vector3.up * speed;
+            direction += Vector3.up * speed;
         if (Input.GetKey(KeyCode.Q))
-            move -= Vector3.up * speed;
-        transform.Translate(move);
+            direction -= Vector3.up * speed;
+        transform.Translate(direction);
  
         if (Input.GetMouseButtonDown(1))
         {
-            anchorPoint = new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
-            anchorRot = transform.rotation;
+            curr = new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+            Rotation_amount = transform.rotation;
         }
         if (Input.GetMouseButton(1))
         {
-            Quaternion rot = anchorRot;
-            Vector3 dif = anchorPoint - new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
-            rot.eulerAngles += dif * sensitivity;
-            transform.rotation = rot;
+            Quaternion Rotation = Rotation_amount;
+            Vector3 dif = curr - new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+            Rotation.eulerAngles += dif * sensitivity;
+            transform.rotation = Rotation;
         }
     }
 }
