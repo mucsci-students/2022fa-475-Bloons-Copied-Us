60d4b4dddcf1651d62fd6435687cfc9289420501
Justin <jmms5757@gmail.com>
2022-11-17 02:08:56 -0500

    worked on stuff

---
+++ Assets/Scripts/TowerScripts/FireMode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FireMode
+{
+
+    public const int FIRST = 0;
+    public const int LAST = 1;
+    public const int STRONGEST = 2;
+    public const int AI = 3;
+
+}
