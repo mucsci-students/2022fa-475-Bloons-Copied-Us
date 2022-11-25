e3352cd2ecbd97e1fbbf1cd095899a8a2d7d9bf0
CodeDog3 <96511553+CodeDog3@users.noreply.github.com>
2022-11-24 23:40:01 -0500

    can place 1 turrent, cant place 2, dont know why

--- Assets/Scripts/UI/OpenStore.cs
+++ Assets/Scripts/UI/OpenStore.cs
@@ -8,20 +8,21 @@ public class OpenStore : MonoBehaviour
 
     //gridmap display when click on tower to buy
     [SerializeField] GameObject Gridmap;
+
     [SerializeField] GameObject Ballista;
-    [SerializeField] GameObject MultiBallista;
-    [SerializeField] GameObject FireTower;
-    [SerializeField] GameObject IceTower;
-    [SerializeField] GameObject LightningTower;
+    // [SerializeField] GameObject MultiBallista;
+    // [SerializeField] GameObject FireTower;
+    // [SerializeField] GameObject IceTower;
+    // [SerializeField] GameObject LightningTower;
     [SerializeField] GameObject PortalTower;
     // multiply prices by complexity? 
     // upgrade prices later
-    [SerializeField] int BallistaPrice = 5;
-    [SerializeField] int MultiBallistaPrice = 10;
-    [SerializeField] int FireTowerPrice = 15;
-    [SerializeField] int IceTowerPrice = 20;
-    [SerializeField] int LightningTowerPrice = 25;
-    [SerializeField] int PortalTowerPrice = 30;
+    int BallistaPrice = 5;
+    int MultiBallistaPrice = 10;
+    int FireTowerPrice = 15;
+    int IceTowerPrice = 20;
+    int LightningTowerPrice = 25;
+    int PortalTowerPrice = 30;
     //*********************************************
     // open and close store
     public void storeOpen()
@@ -31,32 +32,46 @@ public class OpenStore : MonoBehaviour
     public void storeClose()
     {
         if (!Pause.isPaused) storeui.SetActive(false);
+        Gridmap.SetActive(false);
     }
     //**********************************************
     // tower buying
+    public static GameObject tower;
+    public static int towerprice = 0;
+
     public void buyBallista()
     {
         Gridmap.SetActive(true);
+        tower = Ballista;
+        towerprice = BallistaPrice;
+        UpOnHover.placed = false;
     }
-    public void buyMultiBallista()
-    {
-        Gridmap.SetActive(true);
-    }
-    public void buyFireTower()
-    {
-        Gridmap.SetActive(true);
-    }
-    public void buyIceTower()
-    {
-        Gridmap.SetActive(true);
-    }
-    public void buyLightningTower()
-    {
-        Gridmap.SetActive(true);
-    }
+    // public void buyMultiBallista()
+    // {
+    //     Gridmap.SetActive(true);
+    //     TowerID = 2;
+    // }
+    // public void buyFireTower()
+    // {
+    //     Gridmap.SetActive(true);
+    //     TowerID = 3;
+    // }
+    // public void buyIceTower()
+    // {
+    //     Gridmap.SetActive(true);
+    //     TowerID = 4;
+    // }
+    // public void buyLightningTower()
+    // {
+    //     Gridmap.SetActive(true);
+    //     TowerID = 5;
+    // }
     public void buyPortalTower()
     {
         Gridmap.SetActive(true);
+        tower = PortalTower;
+        towerprice = PortalTowerPrice;
     }
+
 
 }
